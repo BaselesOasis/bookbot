0fb255d8331c4fc156222d7d004813cff7a8339d
BaslesOasis <jda.mueller@yahoo.com>
2025-05-23 22:52:43 +0200

    updated main.py to allow for diffrent book paths.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import sys
+
 from stats import get_word_count
 from stats import get_char_count
 
@@ -11,22 +13,35 @@ def sort_char_dict(char_dict):
     return sorted_char_dict
 
 
-def main(relativ_book_path):
-    book_txt = get_book_txt(relativ_book_path)
-    num_words = get_word_count(book_txt)
-    char_dict = get_char_count(book_txt)
-    sorted_char_dict = sort_char_dict(char_dict)
+def main():
+    if len(sys.argv) > 1:
+        relativ_book_path = sys.argv[1]
+        try:
 
-    print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
-    print("----------- Word Count ----------")
-    print(f"Found {num_words} total words")
-    print("--------- Character Count -------")
-    for key in sorted_char_dict:
-        print(f"{key}: {sorted_char_dict[key]}")
-    print("============= END ===============")
+            book_txt = get_book_txt(relativ_book_path)
+            num_words = get_word_count(book_txt)
+            char_dict = get_char_count(book_txt)
+            sorted_char_dict = sort_char_dict(char_dict)
+
+            print("============ BOOKBOT ============")
+            print("Analyzing book found at books/frankenstein.txt...")
+            print("----------- Word Count ----------")
+            print(f"Found {num_words} total words")
+            print("--------- Character Count -------")
+            for key in sorted_char_dict:
+                print(f"{key}: {sorted_char_dict[key]}")
+            print("============= END ===============")
+        except FileNotFoundError:
+            print(f"Error: no file found in {relativ_book_path}")
+        except Exception as e:
+            print("Error: unexpected error occoured: {e}")
+
+    else:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
 
     
-main("books/frankenstein.txt")
+main()
     
         
